8ee852bcffc34d234b339cf837d34c6683c33884
JayrajsinhParmar <jayrajsinh.parmar@ifour.com>
2020-03-20 18:53:50 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { CommonLoginComponent } from './common-login/common-login.component';
-import { CommonSidebarComponent } from './common-sidebar/common-sidebar.component';
-import { UserRegisterComponent } from './user-register/user-register.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminDashboardComponent } from './admin/admin/dashboard/admin-dashboard.component';
 
 
 const routes: Routes = [
